d07035b0a89332b250090eb44fd63e643ad4eae4
Amr A. Abdelmoez <amradel88@live.com>
2020-07-25 01:17:37 +0200

    Optimize background images & add serviceWorker

---
+++ service-worker.js
@@ -0,0 +1,30 @@
+const cacheName = "colorName-testing-2";
+
+const precacheResources = [
+  "/",
+  "index.html",
+  // CSS
+  "css/style.css",
+  // Images
+  "images/icons.svg",
+  "images/color-app-palettes-icons.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(precacheResources);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    })
+  );
+});
